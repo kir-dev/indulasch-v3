160b7bec87d974186a9622ed4028f1e2af4e0b5a
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-01-03 21:21:08 +0100

    SSO Login page text

--- packages/admin/src/pages/SsoLogin.page.tsx
+++ packages/admin/src/pages/SsoLogin.page.tsx
@@ -1,5 +1,5 @@
 import { Page } from '../layout/Page';
-import { CardBody, VStack } from '@chakra-ui/react';
+import { CardBody, Text, VStack } from '@chakra-ui/react';
 import { NavButton } from '../components/NavButton';
 import { ApiPaths } from '../config/paths.config';
 import { API_BASE_URL } from '../config/environment.config';
@@ -8,9 +8,12 @@ export function SsoLoginPage() {
   return (
     <Page title='Bejelentkezés' maxW='md'>
       <CardBody>
-        <VStack>
+        <VStack spacing={10}>
+          <Text>
+            Jelentkezz be AuthSch fiókoddal! A profilod automatikusan létrejön az első bejelentkezés alkalmával.
+          </Text>
           <NavButton to={API_BASE_URL + ApiPaths.LOGIN} external>
-            AuthSch
+            Bejelentkezés AuthSch-val
           </NavButton>
         </VStack>
       </CardBody>
