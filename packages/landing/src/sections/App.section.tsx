0ed246b2c277917150ba1441f1e307c295bbdcfd
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-03 16:59:57 +0200

    feat: play store added

--- packages/landing/src/sections/App.section.tsx
+++ packages/landing/src/sections/App.section.tsx
@@ -1,4 +1,4 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Heading, HStack, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,9 +12,14 @@ export function AppSection() {
         Indulási idők egy pillantásra. Az InduláSch App a letisztultságra és a gyorsaságra fókuszálva listázza a
         helyzetedhez közeli járatindulásokat. Ne késd le mégegyszer azt a bizonyos buszt!
       </Text>
-      <Link href='https://kir-dev.hu/ly/IIWQJ'>
-        <Image src='/appstore.svg' alt='Letöltés at AppStore-ból' width={150} height={40} />
-      </Link>
+      <HStack>
+        <Link href='https://kir-dev.hu/ly/IIWQJ'>
+          <Image src='/appstore.svg' alt='Letöltés az AppStore-ból' width={150} height={40} />
+        </Link>
+        <Link href='https://kir-dev.hu/ly/tmExk'>
+          <Image src='/playstore.svg' alt='Letöltés a PlayStore-ból' width={133} height={40} />
+        </Link>
+      </HStack>
     </Section>
   );
 }
