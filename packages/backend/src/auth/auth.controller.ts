ffef23702f76cceeeaaefd1a36c61f2369d6850a
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-01-21 16:07:59 +0100

    chore: removed unused config

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,20 +1,9 @@
 import { Controller, Get, Request, Res, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ConfigParams } from 'express-openid-connect';
 
 import { ConfigKeys } from '../utils/configuration';
 import { AuthService } from './auth.service';
 import { OauthGuard } from './oauth.guard';
-
-const config: ConfigParams = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3002',
-  clientID: 'sbFVcgS0dc7VDMHLT8amUw7BegNeVf6T',
-  issuerBaseURL: 'https://kir-dev.eu.auth0.com',
-  authorizationParams: {},
-};
 
 @Controller('admin/auth')
 export class AuthController {
